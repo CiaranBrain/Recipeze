08c95c97f16fa845fa013f3c5a19e8bd8c6eaa90
Ciaran Brain <ciaranb333@gmail.com>
2024-03-13 11:20:07 +0000

    added allauth, added account templates to templates folder, ammend login.html

--- config/urls.py
+++ config/urls.py
@@ -19,6 +19,7 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("accounts/", include("allauth.urls")),
     path('', include('home.urls')),
     path('test/', include('recipe.urls')),
 ]
