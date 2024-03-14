2f73c6900881a54b416ecdc324052add4ac5c002
Ciaran Brain <ciaranb333@gmail.com>
2024-03-14 19:38:13 +0000

    chanegd detialview in recipe view and added html for styling/layout of details

--- recipe/urls.py
+++ recipe/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.recipe_list, name='recipes'),
-    path('<int:pk>/', views.recipe_detail, name='recipe_detail'),
+    path('<int:recipe_id>/', views.recipe_detail, name='recipe_detail'),
 ]
--- recipe/views.py
+++ recipe/views.py
@@ -4,12 +4,30 @@ from .models import Recipe
 
 # Create your views here.
 
+# def recipe_list(request):
+#     recipes = Recipe.objects.all().order_by('created_on')
+#     context = {'recipes': recipes}
+#     return render(request, './recipes_list.html', context)
+
+# # def recipe_detail(request, pk):
+# #     recipe_object = Recipe.objects.all()
+# #     recipes = get_object_or_404(Recipe, pk=pk)
+# #     context = {'recipes': recipes}
+# #     return render(request, './recipe_detail.html', context)
+
+# def recipe_detail(request, pk):
+#     single_recipes = get_object_or_404(Recipe, pk=pk)
+#     context = {'single_recipe': single_recipe}
+#     return render(request, 'recipe_detail.html', context)
+
+
+    # new
+
 def recipe_list(request):
     recipes = Recipe.objects.all().order_by('created_on')
     context = {'recipes': recipes}
-    return render(request, './recipes_list.html', context)
+    return render(request, 'recipes_list.html', context)
 
-def recipe_detail(request, pk):
-    recipes = get_object_or_404(Recipe, pk=pk)
-    context = {'recipes': recipes}
-    return render(request, './recipe_detail.html', context)
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, pk=recipe_id)
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
