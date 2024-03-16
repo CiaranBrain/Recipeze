d8c05db5a7ad52b104da376751d36c4a821c4a1f
Ciaran Brain <ciaranb333@gmail.com>
2024-03-16 01:01:56 +0000

    collect static and debug false

---
+++ staticfiles/js/comments.js
@@ -0,0 +1,43 @@
+// const editButtons = document.getElementsByClassName("btn-edit");
+// const commentText = document.getElementById("id_body");
+// const commentForm = document.getElementById("commentForm");
+// const submitButton = document.getElementById("submitButton");
+
+// const deleteModal = new bootstrap.Modal(document.getElementById("deleteModal"));
+// const deleteButtons = document.getElementsByClassName("btn-delete");
+// const deleteConfirm = document.getElementById("deleteConfirm");
+
+// for (let button of editButtons) {
+//   button.addEventListener("click", (e) => {
+//     let commentId = e.target.getAttribute("data-comment_id");
+//     let commentContent = document.getElementById(`comment${commentId}`).innerText;
+//     commentText.value = commentContent;
+//     submitButton.innerText = "Update";
+//     commentForm.setAttribute("action", `edit_comment/${commentId}`);
+//   });
+// }
+
+// for (let button of deleteButtons) {
+//   button.addEventListener("click", (e) => {
+//     let commentId = e.target.getAttribute("data-comment_id");
+//     deleteConfirm.href = `delete_comment/${commentId}`;
+//     deleteModal.show();
+//   });
+// }
+
+document.querySelectorAll('.btn-edit').forEach(function(button) {
+  button.addEventListener('click', function() {
+      let commentId = this.getAttribute('data-comment_id');
+      let commentContent = document.getElementById(`comment${commentId}`).innerText;
+      document.getElementById('id_body').value = commentContent;
+      document.getElementById('commentForm').setAttribute('action', `edit_comment/${commentId}`);
+      document.getElementById('submitButton').innerText = 'Update';
+  });
+});
+
+document.querySelectorAll('.btn-delete').forEach(function(button) {
+  button.addEventListener('click', function() {
+      let commentId = this.getAttribute('data-comment_id');
+      document.getElementById('deleteConfirm').href = `delete_comment/${commentId}`;
+  });
+});
